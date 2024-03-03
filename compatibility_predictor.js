70651a2170852ce82e0aa2e1955514eb9d20a900
pmmanuel315 <pmmanuel315@gmail.com>
2024-03-02 16:06:19 -1000

    can read json file

--- compatibility_predictor.js
+++ compatibility_predictor.js
@@ -1,5 +1,8 @@
-function CompatibilityPredictor(){
-    console.log("hello world");
+import fs from 'fs';
+
+const inputData = JSON.parse(fs.readFileSync('input_data.json','utf8'));
+function CompatibilityPredictor(inputData){
+    console.log(inputData);
 }
 
-CompatibilityPredictor();
+CompatibilityPredictor(inputData);
