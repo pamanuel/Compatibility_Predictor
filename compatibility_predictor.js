5f0b976348da9149f608eedec8325946e716cb84
pmmanuel315 <pmmanuel315@gmail.com>
2024-03-02 16:17:08 -1000

    set up ScaledTeamAttributes function

--- compatibility_predictor.js
+++ compatibility_predictor.js
@@ -1,8 +1,18 @@
 import fs from 'fs';
-
+import { ScaledTeamAttributes } from './utilities.js'
 const inputData = JSON.parse(fs.readFileSync('input_data.json','utf8'));
 function CompatibilityPredictor(inputData){
-    console.log(inputData);
+    const team = inputData.team;
+    const applicants = inputData.applicants;
+    // console.log(team);
+    // console.log(applicants);
+
+    // Use the team attributes to scale down
+    // get the average of each team member
+    // use average to compare with applicants
+    const avgScaledAttributes = ScaledTeamAttributes(team);
+    return avgScaledAttributes;
 }
 
-CompatibilityPredictor(inputData);
+const output = CompatibilityPredictor(inputData);
+console.log(output);
