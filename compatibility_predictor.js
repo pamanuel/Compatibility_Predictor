2af32bcf8f36a5b74afa115568f0e4664bdbbd42
pmmanuel315 <pmmanuel315@gmail.com>
2024-03-03 12:06:12 -1000

    cleaning

--- compatibility_predictor.js
+++ compatibility_predictor.js
@@ -13,7 +13,7 @@ function CompatibilityPredictor(inputData){
     // get the average of each team member
     // use average to compare with applicants
     const avgTeamAttributeScale = ScaledAvgTeamAttributes(team);
-    console.log(avgTeamAttributeScale);
+    // console.log(avgTeamAttributeScale);
     // const applicantScore = getCompatibilityScore(applicants[0], avgTeamAttributeScale)
     //get compatibility score from each applicant and store into array
     const scoredApplicants = applicants.map(applicant => getCompatibilityScore(applicant, avgTeamAttributeScale));
